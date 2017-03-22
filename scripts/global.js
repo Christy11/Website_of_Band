9d96782851901f90922a4cf4cbb8bb3d5f15982f
Cheery_Wu <601505839@qq.com>
2017-03-22 20:30:28 +0800

    JavaScript图片库-photos.html

--- scripts/global.js
+++ scripts/global.js
@@ -106,7 +106,7 @@ function prepareSlideshow() {
   var slideshow = document.createElement("div");
   slideshow.setAttribute("id", "slideshow");
   var preview = document.createElement("img");
-  preview.setAttribute("src","images/slideshow.png");
+  preview.setAttribute("src","images/slideshow/slideshow.png");
   preview.setAttribute("alt","a glimpse of what awaits you");
   preview.setAttribute("id","preview");
   slideshow.appendChild(preview);
@@ -169,3 +169,57 @@ function prepareInternalnav() {
   }
 }
 addLoadEvent(prepareInternalnav);
+
+function showPic(whichpic) {
+  if (!document.getElementById("placeholder")) return true;
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  placeholder.setAttribute("src",source);
+  if (!document.getElementById("description")) return false;
+  if (whichpic.getAttribute("title")) {
+    var text = whichpic.getAttribute("title");
+  } else {
+    var text = "";
+  }
+
+  var description = document.getElementById("description");
+  if (description.firstChild.nodeType == 3) {
+    description.firstChild.nodeValue = text;
+  }
+  return false;
+}
+function preparePlaceholder() {
+  if (!document.createElement) return false;
+  if (!document.createTextNode) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+
+  var placeholder = document.createElement("img");
+  placeholder.setAttribute("id","placeholder");
+  placeholder.setAttribute("src","images/placeholder.jpg");
+  placeholder.setAttribute("alt","my image gallery");
+
+  var description = document.createElement("p");
+  description.setAttribute("id","description");
+  var desctext = document.createTextNode("Choose an image");
+  description.appendChild(desctext);
+
+  var gallery = document.getElementById("imagegallery");
+  insertAfter(description, gallery);
+  insertAfter(placeholder, description);
+}
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+
+  var gallery = document.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+  for (var i=0; i <links.length; i++) {
+    links[i].onclick = function () {
+      return showPic(this);
+    }
+  }
+}
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
